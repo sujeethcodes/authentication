61c42b9700961e35c80b9a25bb83c971ca8b59a9
sujeethcodes <sujeethcodes@gmail.com>
2024-04-25 17:20:00 +0530

    loginroutChange

--- api/model/account.js
+++ api/model/account.js
@@ -24,6 +24,11 @@ const accounts = DB.define(
       field: "password",
       type: Sequelize.DataTypes.STRING,
     },
+    user:{
+      field: "user",
+      type: Sequelize.DataTypes.STRING,
+      defaultValue: "user" 
+    },
     isAdmin:{
       field: "isAdmin",
       type: Sequelize.DataTypes.BOOLEAN,
